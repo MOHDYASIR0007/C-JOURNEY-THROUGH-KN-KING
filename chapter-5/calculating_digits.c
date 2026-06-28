861071bd107ad5ac432e6439e2b21281b4011b2e
MOHD YASIR <26f2000505@ds.study.iitm.ac.in>
2026-06-28 13:14:41 +0530

    Add files via upload

---
+++ chapter-5/calculating_digits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+//1.
+int main()
+{ int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+     
+    if (0 <= a && a <= 9)
+        printf("The number %d has 1 digit\n", a);
+    else if (10 <= a  && a <= 99)
+        printf("The number %d has 2 digits\n", a);
+    else if (100 <= a && a <= 999)
+        printf("The number %d has 3 digits\n", a);
+    else if (1000 <= a && a <= 9999)
+        printf("The number %d has 4 digits\n", a);
+
+    else printf("dont exceed the input above 9999");
+    
+
+      return 0;
+}
